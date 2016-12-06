5e71535eafdd04b38e4384cc4085982950a29d20
yixiaoming <676893998@qq.com>
2016-12-06 13:33:07 +0800

    add a roletype ROLE_NORMAL

--- src/main/java/org/yxm/jundui/model/RoleType.java
+++ src/main/java/org/yxm/jundui/model/RoleType.java
@@ -4,7 +4,7 @@ package org.yxm.jundui.model;
  * Created by yxm on 2016.11.21.
  */
 public enum RoleType {
-    ROLE_ADMIN("超级管理员"), ROLE_AUDIT("内容编辑"), ROLE_LOOK("内容查看");
+    ROLE_ADMIN("超级管理员"), ROLE_AUDIT("内容编辑"), ROLE_LOOK("内容查看"), ROLE_NORMAL("普通用户");
 
     private String name;
 
